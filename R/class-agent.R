87bfed250cbfe07eccf0d4973ae8ba9414c36615
Bruno Caneco <bruno@dmpstats.co.uk>
2024-11-08 11:46:22 +0000

    updated `Agent-class`

--- R/class-agent.R
+++ R/class-agent.R
@@ -1,45 +1,149 @@
-#' Class `'Agent'`
+#' `<Agent>`
 #'
-#' `'Agent'` is an S4 class specifying properties of the individual agent being
-#' traced in the simulation model. It subsumes the underlying specifics of the
-#' species as well as general features such as current location, time,
-#' condition. In addition, the class allows for the storage of historical data
-#' (movement and other properties of interest)
+#' `<Agent>` is an S4 class representing the biological properties and dynamic
+#' state of an individual agent within the simulation model. It inherits from S4
+#' classes <[AgentProperties-class]> and <[AgentState-class]>, collating a
+#' historical record of the agent's movement and state properties generated
+#' throughout the simulation.
 #'
-#' @include class-species.R
+#' @slot state object of class <[AgentState-class]>, describing the agent's
+#'   state for the current simulation time-step.
+#' @slot properties object of class <[AgentProperties-class]>, defining the
+#'   individual-level properties of the agent
+#' @slot history object of class `sf`, storing historical data comprising the
+#'   agent's state and spatio-temporal activity for each time-step of the
+#'   simulation.
+#'
+#' @include class-AgentState.R class-AgentProperties.R
 #'
 #'
-#' @slot currentLocation object of class `XY`, specifying the current location of
-#'   the agent in terms of (x,y) coordinates
-#' @slot currentTime object of class `POSIXlt`, providing the timestamp at the current
-#'   location
-#' @slot type character string, the name of the class representing the type of agent.
-#'   Defaults to the S4 class [Species-class]
-#' @slot condition a `list` object containing the current condition of the agent.
-#'   Currently is support the following named elements:
-#'   * bodyWt: numeric value, the current body weight of the agent (units: kg)
-#'   * alive: binary, specifying whether the agent is dead (`0`) or alive (`1`)
-#'
-#' @slot history object of class `sf`, to store historical data of the agent
-#'   throughout the simulation
 #' @export
-setClass(
-  "Agent",
+
+methods::setClass(
+  Class = "Agent",
+  #contains = c("AgentProperties", "AgentState"),
   slots = list(
-    currentLocation = "XY",
-    currentTime = "POSIXlt",
-    type = "Species",
-    condition = "list",
+    state = "AgentState",
+    properties = "AgentProperties",
     history = "sf"
   ),
   prototype = list(
-    currentLocation = sf::st_point(),
-    currentTime = as.POSIXlt(NA),
-    type = new("Species"),
-    condition = list(
-      bodyWt = units::set_units(NA, "kg"),
-      alive = NA_integer_
-    ),
-    history = sf::st_sf(timestamp = as.POSIXlt(NA), geom = sf::st_sfc(sf::st_point()))
+    state = new("AgentState"),
+    properties = new("AgentProperties"),
+    history = sf::st_sf(
+      timestamp = as.POSIXct(NA),
+      geom = sf::st_sfc(sf::st_point()),
+      body_mass = NA,
+      foraging = NA,
+      energy = NA,
+      alive = NA
+    )
   )
 )
+
+
+
+
+#' #' Create `<Agent>` objects
+#' #'
+#' #' `Agent()` is a helper function to construct instances of `<Agent>` S4 objects with user-defined values
+#' #'
+#' #' @param location object of class `XY`, specifying the current location of
+#' #'   the agent in terms of (x,y) coordinates
+#' #' @param timestamp object of class `POSIXct`, providing the timestamp at the current
+#' #'   location
+#' #' @param body_mass numeric, the current body weight of the agent (units: kg)
+#' #' @param alive binary, specifying whether the agent is currently dead (`0`) or alive (`1`)
+#' #' @param history object of class `sf`, to store historical data of the agent
+#' #'   throughout the simulation
+#' #' @param species an object of class [Species-class], specifying the
+#' #'   species-level properties of the agent. Use [Species()] to generate the expected object
+#' #'
+#' #' @include class-Species.R
+#' #'
+#' #' @export
+#' Agent <- function(
+#'     timestamp = as.POSIXct(NA),
+#'     location = NA,
+#'     body_mass = NA,
+#'     alive = NA,
+#'     species = NA,
+#'     age = NA,
+#'     sex = NA,
+#'     history = NA){
+#'
+#'
+#'   # Handling NULL and NA inputs --------------
+#'   if(is.na(history)){
+#'     history <- sf::st_sf(timestamp = as.POSIXct(NA), geom = sf::st_sfc(sf::st_point()))
+#'   }
+#'
+#'   if(is.na(location)){
+#'     location <- sf::st_point()
+#'   }
+#'
+#'   if(is.na(species)){
+#'     species = new("Species")
+#'   }
+#'
+#'   # Ensure NA types are correct
+#'   if(is.na(alive)) alive <- NA_integer_
+#'   if(is.na(bodyWt)) bodyWt <- NA_real_
+#'
+#'
+#'   # input validation ----------------------------
+#'   if(!is(species, "Species")){
+#'     cli::cli_abort(c(
+#'       "{.arg species} must be an object of class {.cls Species}",
+#'       "x" = "You've supplied a {.cls {class(species)}} object",
+#'       "i" = "Use {.code Species()} to generate a {.cls Species} object"
+#'     ))
+#'   }
+#'
+#'   if(!is.numeric(alive)){
+#'     cli::cli_abort(
+#'       "Argument {.arg alive} must be {.cls numeric}, not {.cls {class(alive)}}"
+#'       )
+#'   }else if(alive %notin% c(NA_integer_, 0, 1)){
+#'     cli::cli_abort(
+#'       "{.arg alive} must be either 0 or 1, not `{.val {alive}}`")
+#'   }
+#'
+#'
+#'   if(!is.numeric(body_mass)){
+#'     cli::cli_abort(
+#'       "Argument {.code body_mass} must be {.cls numeric}, not {.cls {class(body_mass)}}")
+#'   }
+#'
+#'
+#'   # Defaults for NULL arguments --------------
+#'   if(is.null(history)){
+#'     history <- sf::st_sf(timestamp = as.POSIXct(NA), geom = sf::st_sfc(sf::st_point()))
+#'   }
+#'
+#'   if(is.null(location)){
+#'     location <- sf::st_point()
+#'   }
+#'
+#'
+#'   # construct a new instance of <Agent> ----------
+#'   methods::new(
+#'     Class = "Agent",
+#'     location = location,
+#'     timestamp = timestamp,
+#'     species = species,
+#'     condition = list(
+#'       body_mass = body_mass,
+#'       alive = alive
+#'     ),
+#'     history = history
+#'   )
+#' }
+
+
+
+
+
+
+
+
