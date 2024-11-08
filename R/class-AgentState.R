6f5e1c1d6d4cb63efddc5a7a000d0d115d2cd6a2
Bruno Caneco <bruno@dmpstats.co.uk>
2024-11-08 11:44:26 +0000

    updated "agentState-class"

--- R/class-AgentState.R
+++ R/class-AgentState.R
@@ -1,28 +1,29 @@
 #' `<AgentState>`
 #'
 #' `AgentState` is an S4 class for specifying an agent's current state in terms
-#' of spatio-temporal location and physiological condition at the end of the
+#' of spatio-temporal activity and physiological condition at the end of the
 #' current simulation `time_step` (e.g. end of day). It stores information such
 #' as body mass, activity budgets, energy expenditure metrics, survival status,
 #' etc
 #'
-#' @slot location object of class `XY`, specifying the agent's current spatial
-#'   coordinates (units/projection: TBD).
+#' @slot location object of class `XY`, specifying the agent's current location
+#'   (units/projection: TBD).
 #' @slot grid_cell object of class `XY`, indicating the spatial coordinates of
 #'   the grid-cell occupied by the agent
 #' @slot time_step integer, the current simulation time-step
-#' @slot body_mass numeric, the agent's current body mass (grams)
+#' @slot date `Date` object, the calendar date of the current simulation time-step
+#' @slot mass numeric, the agent's current body mass (units: g)
 #' @slot activity_budget list, defining the agent's time-in-activity budget in
 #'   the current time-step. Each list element represents an activity and the
-#'   time the agent spends on it during this time-step (hours/time-step).
+#'   time the agent spends on it during this time-step (units: hours/time-step).
 #' @slot activity_costs list, specifying the energy costs associated with each
-#'   activity in argument `activity_budget` (KJ/hour/g)
+#'   activity in argument `activity_budget` (units: KJ/hour/g)
 #' @slot energy_expenditure numeric, total energy spent on all activities by the
-#'   end of the current time-step (KJ/g)
+#'   end of the current time-step (units: KJ/g)
 #' @slot foraging_success numeric, the mass of prey consumed per time-step
-#'   (g/time-step)
+#'   (units: g/time-step)
 #' @slot mass_change_value numeric, specifying the change in the agent’s mass
-#'   over the time-step (grams)
+#'   over the time-step (units: g)
 #' @slot mortality_prob numeric, the probability of the agent dying within the
 #'   the current time-step
 #' @slot alive integer, binary indicator of the agent's survival status (`1` if
@@ -38,7 +39,8 @@ methods::setClass(
     location = "XY",
     grid_cell = "XY",
     time_step = "integer",
-    body_mass = "numeric",
+    date = "Date",
+    mass = "numeric",
     activity_budget = "list",
     activity_costs = "list",
     energy_expenditure = "numeric",
@@ -52,7 +54,8 @@ methods::setClass(
     location = sf::st_point(),
     grid_cell = sf::st_point(),
     time_step = NA_integer_,
-    body_mass =  NA_real_,
+    date = as.Date(NA),
+    mass =  NA_real_,
     activity_budget = list(
       flight = NA,
       water_active = NA,
