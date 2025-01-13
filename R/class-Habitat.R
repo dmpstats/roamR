8c3131effed3717b27eb34441a92e57a7a9c0cd9
Bruno Caneco <bruno@dmpstats.co.uk>
2025-01-13 16:19:09 +0000

    <Habitat> update
    - renamed salinity slot
    - changed expected class for terrain (XY to "POLYGON" or "MULTIPOLYGON")

--- R/class-Habitat.R
+++ R/class-Habitat.R
@@ -4,19 +4,19 @@
 #'
 #' `<Habitat>` is an S4 class that encapsulates the Habitat-level features of
 #' the geographical area within the IBM's area of calculation (AOC). These
-#' features are designed to interact with simulated Agents, influencing
-#' their spatial movement, distribution, behaviour and physiological condition
-#' over time
+#' features are designed to interact with simulated Agents, influencing their
+#' spatial movement, distribution, behaviour and physiological condition over
+#' time
 #'
-#' @slot terrain object of class `<XY>` or `<POLYGON>`, representing land
-#'   boundaries (e.g. mainland and/or islands) within the AOC
+#' @slot terrain object of class `sf` or `sfc`, `sfc_POLYGON`, containing
+#'   coastline polygon(s) within the AOC
 #' @slot bathymetry object of class `<stars>`, specifying the bathymetric data
 #'   within the AOC
 #' @slot prey object of class `<stars>`, providing prey density maps within the
 #'   AOC, at discrete time points throughout the simulated period
 #' @slot sst object of class `<stars>`, containing sea surface temperature maps
 #'   within the AOC, at discrete time points  throughout the simulated period
-#' @slot salinity object of class `<stars>`, containing sea salinity maps within
+#' @slot sss object of class `<stars>`, containing sea surface salinity maps within
 #'   the AOC, at discrete time points throughout the simulated period
 #' @slot wind object of class `<stars>`, containing rasters of wind speed and
 #'   direction within the AOC, at discrete time points throughout the simulated
@@ -31,19 +31,19 @@
 methods::setClass(
   Class = "Habitat",
   slots = list(
-    terrain = "XY",
+    terrain = "sfc",
     bathymetry = "stars",
     prey = "Species",
     sst = "stars",
-    salinity = "stars",
+    sss = "stars",
     wind = "stars"
   ),
   prototype = list(
-    terrain = sf::st_polygon(),
+    terrain = sf::st_polygon() |> sf::st_sfc(),
     bathymetry = stars::st_as_stars(matrix(NA)),
     prey = Species(),
     sst = stars::st_as_stars(matrix(NA)),
-    salinity = stars::st_as_stars(matrix(NA)),
+    sss = stars::st_as_stars(matrix(NA)),
     wind = stars::st_as_stars(matrix(NA))
   )
 )
@@ -56,15 +56,15 @@ methods::setClass(
 #'
 #' Helper function to construct instances of <[`Habitat-class`]> objects
 #'
-#' @param terrain object of class `<XY>` or `<POLYGON>`, representing land
-#'   boundaries (e.g. mainland and/or islands) within the AOC
+#' @param terrain object of class `sf` or `sfc`, `sfc_POLYGON`, containing
+#'   coastline polygon(s) within the AOC.
 #' @param bathymetry object of class `<stars>`, specifying the bathymetric data
 #'   within the AOC
 #' @param prey object of class `<stars>`, providing prey density maps within the
 #'   AOC, at discrete time points throughout the simulated period
 #' @param sst object of class `<stars>`, containing sea surface temperature maps
 #'   within the AOC, at discrete time points  throughout the simulated period
-#' @param salinity object of class `<stars>`, containing sea salinity maps
+#' @param sss object of class `<stars>`, containing sea salinity maps
 #'   within the AOC, at discrete time points throughout the simulated period
 #' @param wind object of class `<stars>`, containing rasters of wind speed and
 #'   direction within the AOC, at discrete time points throughout the simulated
@@ -74,22 +74,33 @@ Habitat <- function(terrain = NULL,
                     bathymetry = NULL,
                     prey = NULL,
                     sst = NULL,
-                    salinity = NULL,
+                    sss = NULL,
                     wind = NULL){
 
-  # Null imput handling
-  if(is.null(terrain)) terrain <- sf::st_polygon()
+  # Null input handling
+  if(is.null(terrain)) terrain <- sf::st_polygon() |> sf::st_sfc()
   if(is.null(bathymetry)) bathymetry <- stars::st_as_stars(matrix(NA))
-  if(is.null(prey)) prey <- Species()
+  if(is.null(prey)) prey <- Species(role = "prey")
   if(is.null(sst)) sst <- stars::st_as_stars(matrix(NA))
-  if(is.null(salinity)) salinity <- stars::st_as_stars(matrix(NA))
+  if(is.null(sss)) sss <- stars::st_as_stars(matrix(NA))
   if(is.null(wind)) wind <- stars::st_as_stars(matrix(NA))
 
+  # input validation  ---------------------------------
+  if(is(terrain, "sf")) terrain <- sf::st_geometry(terrain) # extract sfc component from sf
+  check_class(terrain, class = c("sfc"))
   check_class(bathymetry, class = "stars")
   check_class(prey, class = "Species")
   check_class(sst, class = "stars")
-  check_class(salinity, class = "stars")
+  check_class(sss, class = "stars")
   check_class(wind, class = "stars")
+
+  if(!inherits(terrain, c("sfc_POLYGON", "sfc_MULTIPOLYGON"))){
+    cli::cli_abort(c(
+      "{.cls sf/sfc} object passed on to {.arg terrain} must contain simple feature(s) of type `POLYGON` or `MULTIPOLYGON`",
+      "x" = "Object contains feature(s) of type {.code {sf::st_geometry_type(terrain)}}"
+    ))
+  }
+
 
 
   new(
@@ -98,7 +109,7 @@ Habitat <- function(terrain = NULL,
     bathymetry = bathymetry,
     prey = prey,
     sst = sst,
-    salinity = salinity,
+    sss = sss,
     wind = wind
   )
 }
