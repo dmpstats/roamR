25d0bcda4c3a4aec63aa3c00f30365503eb044a9
Bruno Caneco <bruno@dmpstats.co.uk>
2025-02-10 22:35:44 +0000

    Update test-utils.R

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,10 +3,10 @@ test_that("`is_stars_empty()` behaves as expected", {
   expect_true(is_stars_empty(stars::st_as_stars(matrix(NA))))
 
   expect_false(is_stars_empty(stars::st_as_stars(matrix(0))))
-  expect_false(is_stars_empty(drv_sss@stars_obj))
+  expect_false(is_stars_empty(rover_drivers$drv_sss@stars_obj))
 
   expect_false(
-    drv_sss@stars_obj |>
+    rover_drivers$drv_sss@stars_obj |>
     split("months") |>
       is_stars_empty()
     )
