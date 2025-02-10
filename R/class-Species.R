9c9e61813ac715879f2a8e638af68ed2cded39a6
Bruno Caneco <bruno@dmpstats.co.uk>
2025-02-10 22:44:08 +0000

    <Species> update: dropped slot impact_responses

--- R/class-Species.R
+++ R/class-Species.R
@@ -21,17 +21,20 @@
 #'   of the density surfaces
 #' @slot behaviour_profile a list comprising objects of class
 #'   [BehaviourSpec-class], defining the behavioural profile of the species.
-#' @slot impact_responses a list containing objects of class
-#'   [ImpactResponse-class], specifying per-agent responses to impact factors.
+#' @param driver_responses a list of [DriverResponse-class] objects. Each object
+#'   specifies the responses of individual agents to various factors, including
+#'   environmental, biological, or artificial influences, that drive their
+#'   movement and activities. See [DriverResponse-class] class definition for
+#'   further details.
 #' @slot mortality_thresh_distr an object of class [VarDist-class], specifying
 #'   the values of agent's condition (e.g. body mass), below which the agent is
 #'   assumed to have died
 #'
-#' @include class-VarDist.R class-ImpactResponse.R
+#' @include class-VarDist.R
 #'
 #' @seealso
 #'  * Helper function [Species()] to define `<Species>` objects
-#'  * Helper functions [VarDist()], [ImpactResponse()] and [BehaviourSpec()] for constructing
+#'  * Helper functions [VarDist()], [DriverResponse()] and [BehaviourSpec()] for constructing
 #'   objects of the dependency classes
 #'
 #' @export
@@ -43,10 +46,9 @@ methods::setClass(
     role = "character",
     common_name = "character",
     scientific_name = "character",
-    body_mass_distr = "VarDist",
     spatial_distr = "stars",
+    body_mass_distr = "VarDist",
     behaviour_profile = "list",
-    impact_responses = "list",
     driver_responses = "list",
     mortality_thresh_distr = "VarDist"
   ),
@@ -66,10 +68,6 @@ methods::setClass(
       water_resting = BehaviourSpec(behav = "water_resting"),
       nest_attending = BehaviourSpec(behav = "nest_attending"),
       other = BehaviourSpec(behav = "other")
-    ),
-    impact_responses = list(
-      impact_1 = ImpactResponse(impact_id = "owf_footprint"),
-      impact_2 = ImpactResponse(impact_id = "owf_buffer")
     ),
     driver_responses = list(
       land = new("DriverResponse", driver_id = "land"),
@@ -93,26 +91,32 @@ methods::setClass(
 #' @param id character string, the identifier code for the species
 #' @param common_name character string, the common name of the species
 #' @param scientific_name character string, the scientific name of the species
-#' @param role character string, defining the role of the species in the IBM (one
-#'   of "agent", "prey", "competitor")
-#' @param body_mass_distr an object of class [VarDist-class], defining the species'
-#'   distribution of body mass
+#' @param role character string, defining the role of the species in the IBM
+#'   (one of "agent", "prey", "competitor")
+#' @param body_mass_distr an object of class [VarDist-class], defining the
+#'   species' distribution of body mass
 #' @param spatial_distr a `<stars>` array, comprising a time-series of
 #'   raster-type density surfaces of the species covering the area of interest.
 #'   First 2 dimensions are expected to provide the spatial properties of the
 #'   density surfaces. The 3rd dimension specifies the temporal resolution of
 #'   the data, while the 4th dimension relates to draws (e.g. bootstrap samples)
 #'   of the density surfaces
+#' @param pop_size numeric, the population size within the Area of calculation
 #' @param behaviour_profile a list comprising objects of class
 #'   [BehaviourSpec-class], defining the behavioural profile of the species.
-#' @param impact_responses a list containing objects of class
-#'   [ImpactResponse-class], specifying per-agent responses to impact factors.
+#' @param driver_responses a list of [DriverResponse-class] objects. Each object
+#'   specifies the responses of individual agents to various factors, including
+#'   environmental, biological, or artificial influences, that drive their
+#'   movement and activities. See [DriverResponse-class] class definition for
+#'   further details.
 #' @param mortality_thresh_distr an object of class [VarDist-class], specifying
 #'   the values of agent's condition (e.g. body mass), below which the agent is
 #'   assumed to have died
 #'
+#'   specific environmental or man-made "driver"
+#'
 #' @seealso
-#'  * Helper functions [VarDist()], [ImpactResponse()] and [BehaviourSpec()]
+#'  * Helper functions [VarDist()], [DriverResponse()] and [BehaviourSpec()]
 #'  * [stars::st_as_stars()] for generating `<stars>` arrays
 #'
 #' @examples
@@ -128,31 +132,45 @@ methods::setClass(
 #'     speed = VarDist(dist_uniform(10, 20), "m/s")
 #'     ),
 #'  dive = BehaviourSpec(
-#'     behav = "dive",
+#'     behav = "diving",
 #'     energy_cost = VarDist(dist_uniform(1, 2), units = "kJ/hour/gram"),
 #'     time_budget = VarDist(dist_uniform(1, 3), "hours/day"),
 #'     speed = VarDist(dist_uniform(10, 20), "m/s")
 #'   )
 #' )
 #'
-#' # 2. the list of impact responses
-#' imp_resp <- list(
-#'   owf_footprint = ImpactResponse(
-#'     impact_id = "owf_foot",
-#'     displace_prob = VarDist(dist_beta(2, 8)),
-#'     displace_fn = function(x, y){ x/y},
-#'     disturb_prob = VarDist(dist_beta(1, 5)),
-#'     disturb_behav = "flying",
-#'     # extra hours/day of flight due to impact
-#'     disturb_extent = VarDist(dist_lognormal(2, 1), "hr/day")
-#'   )
-#' )
+#' # 2. Then the list of driver responses
+#' driver_resp <- list(
+#'   coast = new(
+#'     "DriverResponse",
+#'     driver_id = "land",
+#'     movement = MoveInfluence(
+#'       prob = VarDist(distributional::dist_degenerate(1)),
+#'       fn = function(x, slope = 1/2) exp(-slope * x),
+#'       type = "repulsion"
+#'     )
+#'   ),
+#'   owf = new(
+#'     "DriverResponse",
+#'     driver_id = "owf_foot",
+#'     movement = MoveInfluence(
+#'       prob = VarDist(dist_beta(2, 8)),
+#'       fn = function(x, slope = 1/2) exp(-slope * x),
+#'       type = "repulsion"
+#'     ),
+#'     activities = list(
+#'       ActivInfluence(
+#'         behav = "flying",
+#'         prob = VarDist(dist_beta(1, 5)),
+#'         extent = VarDist(dist_lognormal(2, 1), "hr/day")
+#'       ))))
 #'
 #' # 3. `stars` array with 10 samples of monthly density distribution
 #' dens <- data.frame(
-#'            expand.grid(x= 1:5, y = 1:5, month = 3:5, iter = as.integer(1:10)),
-#'            counts = rlnorm(5*5*3*10)) |>
-#'         st_as_stars(dims = c("x", "y", "month", "iter"))
+#'   expand.grid(x= 1:5, y = 1:5, month = 3:5, iter = as.integer(1:10)),
+#'   counts = rlnorm(5*5*3*10)) |>
+#'   st_as_stars(dims = c("x", "y", "month", "iter")
+#'   )
 #'
 #' # 4. specify <Species>
 #' guill <- Species(
@@ -160,11 +178,11 @@ methods::setClass(
 #'      role = "agent",
 #'      common_name = "guillemot",
 #'      scientific_name = "Uria Aalge",
+#'      spatial_distr = dens,
 #'      body_mass_distr = VarDist(dist_uniform(850, 1130), "g"),
 #'      mortality_thresh_distr = VarDist(dist_normal(500, 20), "g"),
-#'      spatial_distr = dens,
 #'      behaviour_profile = behav,
-#'      impact_responses = imp_resp
+#'      driver_responses = driver_resp
 #'    )
 #'
 Species <- function(id = NA_character_,
@@ -175,12 +193,10 @@ Species <- function(id = NA_character_,
                     mortality_thresh_distr = VarDist(),
                     spatial_distr = stars::st_as_stars(matrix(NA)),
                     behaviour_profile = list(),
-                    impact_responses = list(),
                     driver_responses = list()){
 
   # allow for unlisted objects assigned to `behaviour_profile` and `impact_responses`, if of the correct class
   if(is(behaviour_profile, "BehaviourSpec")) behaviour_profile <- list(behaviour_profile)
-  if(is(impact_responses, "ImpactResponse")) impact_responses <- list(impact_responses)
   if(is(driver_responses, "DriverResponse")) driver_responses <- list(driver_responses)
 
   # input validation ---------------
@@ -193,14 +209,13 @@ Species <- function(id = NA_character_,
   check_class(mortality_thresh_distr, "VarDist")
   check_class(spatial_distr, "stars")
   if(length(behaviour_profile) > 0) check_class(behaviour_profile, "BehaviourSpec", inlist = TRUE)
-  if(length(impact_responses) > 0) check_class(impact_responses, "ImpactResponse", inlist = TRUE)
   if(length(driver_responses) > 0) check_class(driver_responses, "DriverResponse", inlist = TRUE)
 
 
   # TODO:
   #   (i) sense check on chosen distributions and parameters given the nature of
   #       the variable. Implied args: body_mass_distr, mortality_thresh_distr
-  #   (ii) ensure behaviours are nor duplicated
+  #   (ii) ensure behaviours are not duplicated
 
 
   # construct a new instance of <Species>
@@ -213,7 +228,6 @@ Species <- function(id = NA_character_,
     mortality_thresh_distr = mortality_thresh_distr,
     spatial_distr = spatial_distr,
     behaviour_profile = behaviour_profile,
-    impact_responses = impact_responses,
     driver_responses = driver_responses
   )
 }
--- R/rmr_initiate.R
+++ R/rmr_initiate.R
@@ -40,3 +40,94 @@ rmr_initiate <- function(species, habitat, structures, config){
   )
 
 }
+#'
+#' Primarily intended For simulation purposes, so that agents are kept within
+#' the area of calculation in the movement model
+#'
+#' @noRd
+generate_aoc_driver <- function(bbox, grid){
+  # TODO: (i) documentation; (ii) unit-testing
+
+  # Cast bbox as linestring, for cell-to-boundary distance calculation
+  bbox <- sf::st_as_sfc(bbox) |> sf::st_cast("LINESTRING")
+
+  # generate grid distances surface
+  grid$bbox_dist <- sf::st_distance(grid, bbox)
+  bbox_grid_dist <- stars::st_rasterize(grid)["bbox_dist"]
+
+  Driver(
+    id = "aoc",
+    type = "model",
+    descr = "Distance to AOC's bounding box",
+    stars_obj = bbox_grid_dist,
+    stars_descr = "Distance to AOC's bounding box",
+    obj_active = "stars"
+  )
+
+}
+
+
+#' Compute vector field of a stars raster
+#'
+#' Calculate the vector fields (rasters aspect and slope) of a stars object for
+#' a single attribute, for multi-dimensions
+compute_vector_fields <- function(strs){
+
+  if(!inherits(strs, "stars")) cli::cli_abort("{arg. strs} must be a {.cls stars} object")
+  if(length(strs) != 1) stop("'strs' must be a single-attribute <stars> object")
+
+  # get the labels of dimensions defining the coords of the spatial grid,
+  # i.e. the names used for the x/y dimensions
+  xy_labs <- attr(stars::st_dimensions(strs), "raster")$dimensions
+  # dimnames for non-grid variables
+  cov_labs <- setdiff(dimnames(strs), xy_labs)
+
+  if(length(cov_labs) == 0){
+    vfs <- get_slope_aspect(strs)
+  }else{
+    cov_vals <- sapply(
+      cov_labs,
+      \(x) stars::st_get_dimension_values(strs, which = x),
+      simplify = FALSE
+    )
+
+    cov_grid <- expand.grid(cov_vals, stringsAsFactors = FALSE)
+
+    # compute vector fields for each layer
+    vfs_ls <- purrr::pmap(cov_grid, \(...){
+      cov_val <- list(...)
+      slice_strs(strs, cov_labs, !!!cov_val, .drop = TRUE) |>
+        get_slope_aspect()
+    })
+
+    # combine single layers into original datacube
+    vfs <- do.call("c", append(vfs_ls, list(along = cov_vals)))
+  }
+  vfs
+}
+
+
+
+# Calculates slope and aspect of one attribute in the stars object and
+# binds them to the original stars object as attributes
+get_slope_aspect <- function(strs){
+  #browser()
+  if(!inherits(strs, "stars")) stop("`strs` must be a <stars> object")
+  if(length(strs) != 1) stop("`strs` must be a single-attribute <stars> object")
+  if(length(dim(strs)) > 2) stop("`strs` cannot have more than 2 dimensions")
+
+  vf <- as(strs, "SpatRaster") |>
+    terra::terrain(v = c("aspect", "slope")) |>
+    stars::st_as_stars(as_attributes = TRUE)
+
+  # force equal dimensions of original data
+  stars::st_dimensions(vf) <- stars::st_dimensions(strs)
+
+  c(strs, vf)
+}
+
+
+
+
+
+
