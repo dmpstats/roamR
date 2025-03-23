48e5bf079a3224cf3ef9f7e73f608e20f6366a38
statsgeeknz <statsgeeknz@googlemail.com>
2025-03-23 20:14:40 +0100

    moving move devel folder out

--- movement_devel/sample_dive_time.R
+++
@@ -1,8 +0,0 @@
-sample_dive <- function(n_samp, lmean = -0.2631779, lsd = 0.8344929, t_max = 3.4, t_min = 0.2){
-
-  rlnorm(n_samp, meanlog = lmean, sdlog = lsd) %>%
-    ifelse(. > t_max, t_max, .) %>%
-    ifelse(. < t_min, t_min, .) %>%
-    units::set_units(., min)
-
-}
