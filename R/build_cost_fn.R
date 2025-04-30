9c304b6fb91dfd624c90bb80f8830fc512d1418d
Bruno Caneco <bruno@dmpstats.co.uk>
2025-04-30 12:04:35 +0100

    build_cost_fn updated

--- R/build_cost_fn.R
+++ R/build_cost_fn.R
@@ -11,64 +11,69 @@
 #' The function passed on to `base_fun` needs to observe a bunch of
 #' requirements:
 #'  * Argument names must be the ID of a driver, "b" for biomass and/or "t" for
-#' time spent on activity on a given time-step
+#' time spent on activity on a current time-step
 #'  * it is be responsible to handle the intended units
 #'
-#' @param base_fun a function, the user-defined function for calculating the
+#' @param vrf an `<VarFn>` object, the user-defined function for calculating the
 #'   energy cost of a given state. It gets wrapped by a function that is usable
 #'   within `{roamR}`s infrastructure.
-#' @param drivers a list of `<Driver>` objects.
 #'
 #' @importFrom rlang !!!
-build_cost_fn <- function(base_fn, drivers){
+build_cost_fn <- function(vrf, state_id = NULL){
 
-  if(!is.function(base_fn)) cli::cli_abort("Argument {.arg base_fn} must be a function.")
+  force(vrf)
+  force(state_id)
 
-  force(base_fn)
-  force(drivers)
+  # input validation
+  check_class(vrf, "VarFn")
 
-  # Base function arg names
-  bf_arg_names <- rlang::fn_fmls_names(base_fn)
+  # manufacture function for internal use
+  #
+  # manufactured function computes/extracts input values for each argument of
+  # the user-defined function from the data inputs passed on to the manufactured
+  # function
+  function(agent, drivers, details = FALSE){
 
-  # get driver ids
-  driver_ids <- sapply(drivers, \(x) x@id)
+    check_class(agent, "Agent")
+    check_class(drivers, "Driver", inlist = TRUE)
 
-  # get types of arguments, driver, biomass ("b) or time-spent in state ("t")
-  bf_arg_types <- sapply(bf_arg_names, function(arg){
-    if(arg %in% driver_ids){
-      type <- "driver"
-    } else if (arg %in% c("b", "t")) {
-      type <- arg
-    } else{
-      type <- "invalid"
-    }
-  })
+    arg_inputs <- purrr::imap(vrf@args_spec, function(arg, arg_nm){
 
 
-  # invalid_args <- purrr::keep(bf_arg_types, \(x) x == "invalid")
-  # if(length(invalid_args) > 0){
-  #   cli::cli_abort(
-  #     "Argument{?s} name{?s} {.val {names(invalid_args)}} in base function {?is/are} not valid.",
-  #     class = "err-invalid-base-fn-arg"
-  #   )
-  # }
+      if(arg@type == "driver"){ # driver arg type
 
+        val <- drivers |>
+          purrr::keep(\(d) d@id == arg@driver_id) |>
+          purrr::pluck(1) |>
+          get_driver_cell_value(agent)
 
-  # manufacture function
-  function(drivers, agent){
-
-    bf_arg_inputs <- purrr::map2(
-      bf_arg_names, bf_arg_types,
-      function(arg, type){
-        switch(
-          type,
-          driver = get_driver_cell_value(drivers, arg, agent),
-          b = body_mass(agent) |> as.numeric(),
-          t = 333 #get_time_spent_at_state(agent)
+      }else{  # remaining arg types
+        val <- switch(
+          arg@type,
+          body_mass = body_mass(agent),
+          time_at_state = get_time_spent_at_state(agent, state_id),
+          constant = arg@value,
+          random = get_drawn_cost_par(agent, state_id, arg_nm)
         )
-      })
+      }
 
-    eval(rlang::expr(base_fn(!!!bf_arg_inputs)))
+      # Perform required unit conversions. In practice, this is only
+      # applicable to data-related arguments, such as `driver`, `body_mass` &
+      # `time-at-state`, where discrepancies on unit scales might occur. Units
+      # on other types of args are "self-contained".
+      # Also implies that value takes the units from the fetched data by default
+      # if the arg is specified as unitless (i.e. units = "")
+      if(arg@units != "") units(val) <- arg@units
+
+      # drop units for usage in user-defined function
+      units(val) <- NULL
+      val
+    })
+
+    # evaluates the base function by passing on the computed values
+    out <- eval(rlang::expr(vrf@fn(!!!arg_inputs)))
+
+    if(details) list(output = out, input = arg_inputs) else out
   }
 
 }
@@ -78,47 +83,105 @@ build_cost_fn <- function(base_fn, drivers){
 
 
 
-get_driver_cell_value <- function(drivers, driver_id, agent){
+# placeholder function to compute or extract time spent at state
+get_time_spent_at_state <- function(agent, state_id){
+  runif(1)
+}
 
-  # TODO
-  # - extract value of correct attribute (currently the complement of
-  # c(aspect, slope), but maybe make it specific to driver_id instead, once that
-  # is a requirement in the <Driver> definition?)
-  # - proper filtering of the temporal dimension
-  # - integrate potential presence of bootstrap replicates
-  # - handle NAs returned from st_extract
 
-  driver_stars <- drivers |>
-    purrr::keep(\(d) d@id == driver_id) |>
-    purrr::pluck(1) |>
-    stars_obj() |>
-    dplyr::select(!dplyr::any_of(c("slope", "aspect")))
 
-  if(is_stars_empty(driver_stars)){
-    cli::cli_abort(c(
-      "No raster-type data available for driver {.val {driver_id}}",
-      "x" = "Unable to extract values of {.val {driver_id}} to pass on to the base function"
-    ))
-  }else{
-    loc <- sf::st_coordinates(location(agent) + 1)
-    when <- "December" #agent_condition@timestamp
+# helper to check if a cost parameter value has been drawn for state_id
+get_drawn_cost_par <- function(agent, state_id, arg_nm){
+
+  state_ids <- names(agent@properties@cost_par_draws)
+
+  if(state_id %notin% state_ids){
+    cli::cli_abort("Can't find energy cost parameter for state ID {.val {state_id}}.")
   }
 
-  val <- stars::st_extract(
-    dplyr::filter(driver_stars, months == when),
-    at = loc
-  )
-
-  # garbage collection
-  rm(driver_stars)
-
-  val |> as.numeric()
+  agent@properties@cost_par_draws[[state_id]][[arg_nm]]
 }
 
 
 
 
 
+
+
+# build_cost_fn <- function(vrf, drivers, state_id = NULL){
+#
+#   # input validation
+#   check_class(vrf, "VarFn")
+#   check_class(drivers, "Driver", inlist = TRUE)
+#
+#   force(vrf)
+#   force(drivers)
+#
+#   # get driver ids
+#   driver_ids <- sapply(drivers, \(x) x@id)
+#
+#   # check consistency driver-based arguments and available drivers
+#   driver_args <- purrr::keep(vrf@args_spec, \(x) x@type == "driver")
+#
+#   if(length(driver_args) > 0){
+#
+#     driver_args_names <- names(driver_args)
+#     args_driver_ids <- sapply(driver_args, \(x) x@driver_id)
+#     missing_drivers <- setdiff(args_driver_ids, driver_ids)
+#
+#     if(length(missing_drivers) > 0){
+#       cli::cli_abort(paste0(
+#         "{cli::qty(missing_drivers)} `driver_id`{?s} {.val {missing_drivers}} ",
+#         "assigned to{?, respectively,} argument{?s} {cli::col_blue(driver_args_names)} ",
+#         "{?doesn't/don't} match any of those present in {.arg drivers}."
+#       ))
+#       # cli::cli_abort(c(
+#       #   "All driver-related arguments in {.arg vrf} must be specified in {.arg drivers}.",
+#       #   x = "{cli::qty(missing_drivers)} `driver_id`{?s} {.val {missing_drivers}} associated with argument{?s} {cli::col_blue(driver_args_names)} {?doesn't/don't} match any of those in {.arg drivers}.",
+#       #   i = "Possible {.arg driver_id}s: {.val {driver_ids}}."
+#       # ))
+#     }
+#   }
+#
+#   # Sample one value for each random argument This means that the argument is
+#   # sampled at initialization of each agent, remaining fixed for each agent
+#   # across subsequent IBM simulation
+#   args_spec(vrf) <- lapply(args_spec(vrf), function(arg){
+#     if(arg@type == "random"){
+#       arg@value <- distributional::generate(arg@distr, 1)[[1]]
+#     }
+#     arg
+#   })
+#
+#
+#   # manufacture function for internal use
+#   #
+#   # manufactured function computes/extracts input values for each argument of
+#   # the base function from the data inputs passed on to the manufactured function
+#   function(agent, drivers, details = FALSE){
+#
+#     check_class(agent, "Agent")
+#     check_class(drivers, "Driver", inlist = TRUE)
+#
+#     arg_inputs <- lapply(vrf@args_spec, function(arg){
+#
+#       switch(
+#         arg@type,
+#         driver = get_driver_cell_value(drivers, arg@driver_id, agent),
+#         body_mass = body_mass(agent) |> units::drop_units(),
+#         time_at_state = get_time_spent_at_state(agent, state_id),
+#         constant = arg@value,
+#         random = arg@value # value drawn outside in function's factory environment
+#       )
+#     })
+#
+#     # evaluates the base function by passing on the computed values
+#     out <- eval(rlang::expr(vrf@fn(!!!arg_inputs)))
+#
+#     if(details) list(output = out, input = arg_inputs) else out
+#   }
+#
+# }
 
 
 # build_cost_fn <- function(base_fn, drivers){
--- tests/testthat/test-build_cost_fn.R
+++ tests/testthat/test-build_cost_fn.R
@@ -1,29 +1,85 @@
 test_that("a function is returned", {
   expect_true(
     is.function(
-    build_cost_fn(\(b) b, drivers = list(Driver()))
+    build_cost_fn(VarFn())
     )
   )
 })
 
 
 
+test_that("application to single-argument function works as expected", {
 
-test_that("build_cost_fn() fails when base function has invalid arguments", {
+  a <- Agent(rover, ibm_config_rover)
+  location(a) <- location(a) + 1.5 # hack: relocate to get non-NA SST value
 
-  # non-allowed arguments
-  basefoo <- function(b, w, t, r) b + w * t - r
-  expect_error(
-    build_cost_fn(basefoo, list(Driver())),
-    class = "err-invalid-base-fn-arg"
+  # dependent on driver - SST
+  usr_fn <- function(sst){ 113-(2.75*sst) }
+  state_cost <- VarFn(fn = usr_fn, args_spec = list(sst = "driver"))
+
+  mnf_fn <- build_cost_fn(state_cost)
+
+  expected <- rover_drivers$drv_sst@stars_obj |>
+    dplyr::filter(months == "December") |>
+    stars::st_extract(sf::st_coordinates(location(a))) |>
+    as.numeric() |>
+    #units::drop_units() |>
+    usr_fn()
+
+
+  output <- mnf_fn(a, rover_drivers) |> as.numeric()
+  expect_equal(output, expected)
+
+
+  # dependent on body_mass
+  usr_fn <- function(b){ b^2 }
+  state_cost <- VarFn(fn = usr_fn, args_spec = list(b = "body_mass"))
+
+  mnf_fn <- build_cost_fn(state_cost)
+
+  expect_equal(
+    mnf_fn(a, rover_drivers),
+    usr_fn(body_mass(a)) |> units::drop_units()
   )
 
 
-  # driver without a stars object
-  land_cost <- function(land) land^2
-  final_fn <- build_cost_fn(land_cost, rover_drivers)
-  a <- Agent(rover, ibm_config_rover)
-  expect_error(final_fn(rover_drivers, a))
+  # dependent on a "random" term - alpha
+  usr_fn <- function(alpha){ alpha + 10 }
+  alpha_dist <- distributional::dist_normal(5, 1)
+
+  snooze_cost <- VarFn(
+    fn = usr_fn,
+    args_spec = list(alpha = VarDist(alpha_dist))
+  )
+
+  r <- rover # make a copy of the example species
+  r@states_profile$snooze <- State(id = "snooze", energy_cost = snooze_cost) # add the snooze state
+
+  a <- Agent(r, ibm_config_rover) # initiate agent
+  alpha_draw <- a@properties@cost_par_draws$snooze$alpha |> units::drop_units() # extract the value drawn from alpha_dist
+
+  mnf_fn <- build_cost_fn(snooze_cost, state_id = "snooze")
+
+  expect_equal(
+    mnf_fn(a, rover_drivers),
+    usr_fn(alpha_draw)
+  )
+
+
+  # dependent on a "constant" term - eta
+  usr_fn <- function(eta){ sqrt(eta) }
+
+  state_cost <- VarFn(
+    fn = usr_fn,
+    args_spec = list(ArgSpec(name = "eta", "constant", value = 6))
+  )
+
+  mnf_fn <- build_cost_fn(state_cost, rover_drivers)
+
+  output <- mnf_fn(a, rover_drivers, details = TRUE)
+
+  expect_equal(output$input$eta, 6)
+  expect_equal(output$output, usr_fn(6) )
 
 })
 
@@ -32,51 +88,156 @@ test_that("build_cost_fn() fails when base function has invalid arguments", {
 
 
 
-test_that("build_cost_fn() works as expected", {
+test_that("application to multi-argument works as expected", {
 
-  a <- Agent(rover, ibm_config_rover)
+  a <- Agent(rover, ibm_config_rover) # initiate agent
+  location(a) <- location(a) + 2 # hack: relocate to get non-NA SST value
 
-  # one argument
-  one_arg_cost <- function(sst){
-    113-(2.75*sst)
-  }
+  deg <- rover_drivers$drv_sst@stars_obj |>
+    dplyr::filter(months == "December") |>
+    stars::st_extract(sf::st_coordinates(location(a))) |>
+    units::drop_units() |>
+    as.numeric()
 
-  final_fn <- build_cost_fn(one_arg_cost, rover_drivers)
-  final_fn(rover_drivers, a)
+  mass <- body_mass(a) |> units::drop_units()
 
-  #bench::mark(final_fn(rover_drivers, a), iterations = 1000)
 
   # two arguments
-  two_args_cost <- function(sst, b){
-    if(sst < 5){
-      113-(2.75*sst)
-    }else{
-      113-(2.75*sst) * b
-    }
-  }
+  usr_fn <- function(sst, b){ 113-(2.75*sst) + b }
 
-  final_fn <- build_cost_fn(two_args_cost, rover_drivers)
-  final_fn(rover_drivers, a)
+  state_cost <- VarFn(
+    fn = usr_fn,
+    args_spec = list(
+      sst = "driver",
+      b = "body_mass")
+  )
 
-  # only biomass
-  cost_bio <- function(b)  113-(2.75*b)
-  final_fn <- build_cost_fn(cost_bio, rover_drivers)
-  final_fn(rover_drivers, a)
+  mnf_fn <- build_cost_fn(state_cost, rover_drivers)
 
-  #bench::mark(final_fn(rover_drivers, a))
+  expect_equal(
+    mnf_fn(a, rover_drivers),
+    usr_fn(deg, mass)
+  )
+
+})
 
 
-  uncertaint_cost <- function(b){
-    intcp <- rnorm(1, 113, 10)
-    intcp/(2.75*b)
-  }
 
-  final_fn <- build_cost_fn(uncertaint_cost, rover_drivers)
-  final_fn(rover_drivers, a)
 
+test_that("application to multiple random arguments works as expected", {
+
+  # Four arguments - with random intercept 'int' and random alpha
+  usr_fn <- function(sst, b, int, alpha){ int - (2.75*sst) + b/alpha }
+
+  int_dist <- distributional::dist_uniform(100, 200)
+  alpha_dist <- distributional::dist_uniform(0.1, 10)
+
+  snooze_cost <- VarFn(
+    fn = usr_fn,
+    args_spec = list(
+      sst = "driver",
+      b = "body_mass",
+      alpha = VarDist(alpha_dist),
+      int = VarDist(int_dist)
+    )
+  )
+
+  r <- rover # make a copy of the example species
+  r@states_profile$snooze <- State(id = "snooze", energy_cost = snooze_cost) # add the snooze state, below
+
+  a <- Agent(r, ibm_config_rover) # initiate agent
+  location(a) <- location(a) + 2 # hack: relocate to get non-NA SST value
+
+  deg <- rover_drivers$drv_sst@stars_obj |>
+    dplyr::filter(months == "December") |>
+    stars::st_extract(sf::st_coordinates(location(a))) |>
+    units::drop_units() |>
+    as.numeric()
+
+  mass <- body_mass(a) |> units::drop_units()
+
+
+  alpha_draw <- a@properties@cost_par_draws$snooze$alpha |> units::drop_units() # extract the value drawn from alpha_dist
+  int_draw <- a@properties@cost_par_draws$snooze$int |> units::drop_units() # extract the value drawn from int_dist
+
+  mnf_fn <- build_cost_fn(snooze_cost, state_id = "snooze")
+
+  #rlang::env_print(mnf_fn)$vrf
+  expect_equal(
+    mnf_fn(a, rover_drivers),
+    usr_fn(sst = deg, b =mass, alpha = alpha_draw, int = int_draw)
+  )
+
+})
+
+
+
+test_that("application to multi-argument works as expected", {
+
+  # user-function expects SST in Farenheight
+  usr_fn <- function(sst, b){ 113-(2.75*sst) + b }
+
+  state_cost <- VarFn(
+    fn = usr_fn,
+    args_spec = list(
+      sst = ArgSpec(name = "sst", type = "driver", units = "Fahrenheit"),
+      b = "body_mass")
+  )
+
+
+  a <- Agent(rover, ibm_config_rover) # initiate agent
+  location(a) <- location(a) + 2 # hack: relocate to get non-NA SST value
+
+  deg <- rover_drivers$drv_sst@stars_obj |>
+    dplyr::filter(months == "December") |>
+    stars::st_extract(sf::st_coordinates(location(a))) |>
+    dplyr::pull(1) |>
+    units::set_units("Fahrenheit") |>  # convert Celsius to Fahren
+    units::drop_units() # then drop units to use in user-function
+
+  mass <- body_mass(a) |> units::drop_units()
+
+  # Manufactured function should deal with units conversion internally
+  mnf_fn <- build_cost_fn(state_cost, rover_drivers)
+
+  expect_equal(
+    mnf_fn(a, rover_drivers),
+    usr_fn(deg, mass)
+  )
+
+})
+
+
+
+
+
+
+test_that("fails as expected", {
+
+  r <- rover # make a copy of the example species
+  a <- Agent(r, ibm_config_rover)
+
+  usr_fn <- function(d){ 113-(2.75*d) }
+  state_cost <- VarFn(
+    fn = usr_fn,
+    args_spec = list(ArgSpec("d", type = "random", distr = distributional::dist_burr(1, 1)))
+  )
+
+  mnf_fn <- build_cost_fn(state_cost, state_id = "state")
+
+  expect_error(
+    mnf_fn(a, rover_drivers),
+    "Can't find energy cost parameter for state ID"
+  )
 
 
 })
+
+
+
+
+
+
 
 
 
