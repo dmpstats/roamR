8c9a7d8c0014f6858249be4144b37d3a7513663e
statsgeeknz <statsgeeknz@googlemail.com>
2025-05-27 10:30:22 +0100

    Modify path calc
    Included BC code to snap to nearest valid point in impacted map if final location can't be matched.

--- R/path_calc.R
+++ R/path_calc.R
@@ -1,13 +1,14 @@
 #' Shortest path calc
 #'
 #' @param density_map A raster density map to sample from
+#' @param imp_dens A raster density map under impact to sample from
 #' @param agent A roamR agent - mainly for its current location
 #'
 #' @returns A path
 #' @export
 #'
 #' @examples TBD
-path_calc <- function(density_map, agent) {
+path_calc <- function(density_map, imp_dens, agent) {
 
   zero_path <- T
 
@@ -16,6 +17,16 @@ path_calc <- function(density_map, agent) {
   finish <- roamR::sample_cell(density_map, 1) |>
     sf::st_point() |>
     sf::st_sfc(crs = "epsg:32630")
+
+  imp_end_cell_val <- stars::st_extract(imp_dens, finish)[[1]]
+
+  # if cell value is NA relocate endpoint to the closest populated cell in the impacted density surface
+  if(is.na(imp_end_cell_val)){
+    imp_dens_sf <- sf::st_as_sf(imp_dens, as_points = TRUE)
+    new_end_imp_dens_idx <- sf::st_nearest_feature(finish, imp_dens_sf)
+    finish <- imp_dens_sf$geometry[new_end_imp_dens_idx]
+    rm(imp_dens_sf) # garbage collection
+  }
 
   start <- sf::st_sfc(agent@condition@location, crs =  "epsg:32630")
 
