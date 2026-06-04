cab0446ce970a36d383dc517ef843ce4825d98d3
Bruno Caneco <bruno@dmpstats.co.uk>
2026-06-04 15:37:31 +0100

    <Species>: documentation updates

--- R/class-Species.R
+++ R/class-Species.R
@@ -5,20 +5,18 @@
 #' behavioural/activity states, responses to environmental and non-environmental
 #' drivers, and population size.
 #'
-#' @slot id character string, a unique identifier for the species.
-#' @slot common_name character string, the common name of the species.
-#' @slot scientific_name character string, the scientific name of the species.
+#' @slot id <[`string`][character]> the unique identifier for the species.
+#' @slot common_name <[`string`][character]> the common name of the species.
+#' @slot scientific_name <[`string`][character]> the scientific name of the species.
 #' @slot body_mass_distr an object of class [VarDist-class], defining the
 #'   distribution of body mass for the species.
-#' @slot energy_to_mass_distr a [VarDist-class] object, providing the
-#'   energy-to-bodymass conversion rate for the species (e.g. g/kJ). Determines
-#'   how gained or lost energy translates into changes in body mass.
-#' @slot states_profile a list of [State-class] objects, defining the
+#' @slot energy_to_mass_distr <[`VarDist-class`]> the energy-to-bodymass conversion rate for the species (e.g. g/kJ). Determineshow gained or lost energy translates into changes in body mass.
+#' @slot states_profile <[`list`]> of <[`State-class`]> objects, defining the
 #'   behavioural/activity states for the species to consider in the model.
-#' @slot driver_responses list of [DriverResponse-class] objects, specifying the
+#' @slot driver_responses <[`list`]> of <[`DriverResponse-class`]> objects, specifying the
 #'   species’ responses to predefined model drivers, such as environmental
 #'   pressures, biological influences, and human-induced impacts affecting
-#'   movement and behaviour/activity states. See [DriverResponse-class] for
+#'   movement and behaviour/activity states. See <[`DriverResponse-class`]> for
 #'   details.
 #'
 #'   Each element in the `driver_responses` list must correspond to an existing
@@ -29,10 +27,8 @@
 #'   corresponding [Driver-class] object for each entry of `driver_responses`
 #'   will result in missing or undefined driver effects during the simulation.
 #'
-#' @slot mortality_thresh_distr an object of class [VarDist-class], specifying
-#'   the values of agent's condition (e.g. body mass), below which it is
-#'   assumed to have died (*Note: slot may require further generalization*)
-#' @slot pop_size numeric, the population size (i.e., number of individuals)
+#' @slot mortality_thresh_distr <[`VarDist-class`]> values of agent's condition (e.g. body mass), below which it is assumed to have died (*Note: slot may require further generalization*)
+#' @slot pop_size <[`numeric`]> the population size (i.e., number of individuals)
 #'   within the area of calculation (AOC).
 #'
 #' @include class-VarDist.R
@@ -71,45 +67,38 @@ methods::setClass(
 )
 
 
-
-
 #' Create `<Species>` objects
 #'
-#' Helper function to construct instances of [Species-class] objects, representing
+#' Helper function to construct instances of <[`Species-class`]> objects, representing
 #' species-level properties of simulated agents. These include attributes
 #' related to taxonomy, physiology, behavioural/activity states, responses to
 #' environmental and non-environmental drivers, and population size.
 #'
 #'
-#' @param id character string, a unique identifier for the species.
-#' @param common_name character string, the common name of the species.
-#' @param scientific_name character string, the scientific name of the species.
-#' @param body_mass_distr an object of class [VarDist-class], defining the
-#'   distribution of body mass for the species.
-#' @param energy_to_mass_distr a [VarDist-class] object, providing the
-#'   energy-to-bodymass conversion rate for the species (e.g. g/kJ). Determines
-#'   how gained or lost energy translates into changes in body mass.
-#' @param states_profile a list of [State-class] objects, defining the
+#' @param id <[`string`][character]> the unique identifier for the species.
+#' @param common_name <[`string`][character]> the common name of the species.
+#' @param scientific_name <[`string`][character]> the scientific name of the species.
+#' @param body_mass_distr <[`VarDist-class`]> the distribution of body mass for the species.
+#' @param energy_to_mass_distr <[`VarDist-class`]> the energy-to-bodymass conversion rate for the species (e.g. g/kJ). Determines how gained or lost energy translates into changes in body mass.
+#' @param states_profile <[`list`] of <[`State-class`]> objects, defining the
 #'   behavioural/activity states for the species to consider in the model.
-#' @param driver_responses a list of [DriverResponse-class] objects, specifying
+#' @param driver_responses <[`list`] of <[`DriverResponse-class`]> objects, specifying
 #'   the species’ responses to predefined model drivers, such as environmental
 #'   pressures, biological influences, and human-induced impacts affecting
 #'   movement and behaviour/activity states. See the Details section below and
-#'   [DriverResponse-class] for further information.
+#'   <[`DriverResponse-class`]> for further information.
 #'
-#' @param mortality_thresh_distr an object of class [VarDist-class], specifying
-#'   the values of agent's condition (e.g. body mass), below which it is
-#'   assumed to have died (*Note: parameter may require further generalization*)
-#' @param pop_size numeric, the population size (i.e., number of individuals)
+#' @param mortality_thresh_distr <[`VarDist-class`]> the values of agent's condition (e.g. body mass), below which it is assumed to have died (*Note: parameter may require further generalization*)
+#' @param pop_size <[`numeric`]> the population size (i.e., number of individuals)
 #'   within the area of calculation (AOC).
 #'
 #'
 #' @details
 #' Each element in the `driver_responses` list must correspond to an existing
-#' object of class [Driver-class]. At the initialization phase of `{roamR}`s
+#' object of class <[`Driver-class`]> At the initialization phase of `{roamR}`s
 #' IBM, these `driver_responses` are matched with their respective
-#' [Driver-class] objects to ensure a consistent set of species-driver
-#' interactions. Failure to provide a corresponding [Driver-class] object for
+#' <[`Driver-class`]> objects to ensure a consistent set of species-driver
+#' interactions. Failure to provide a corresponding <[`Driver-class`]> object for
 #' each entry of `driver_responses` will result in missing or undefined driver
 #' effects during the simulation.
 #'
@@ -175,19 +164,24 @@ methods::setClass(
 #'      driver_responses = driver_resp
 #'    )
 #'
-Species <- function(id = NA_character_,
-                    common_name = NA_character_,
-                    scientific_name = NA_character_,
-                    body_mass_distr = VarDist(),
-                    energy_to_mass_distr = VarDist(),
-                    mortality_thresh_distr = VarDist(),
-                    states_profile = list(),
-                    driver_responses = list(),
-                    pop_size = NA_real_){
-
+Species <- function(
+  id = NA_character_,
+  common_name = NA_character_,
+  scientific_name = NA_character_,
+  body_mass_distr = VarDist(),
+  energy_to_mass_distr = VarDist(),
+  mortality_thresh_distr = VarDist(),
+  states_profile = list(),
+  driver_responses = list(),
+  pop_size = NA_real_
+) {
   # allow for unlisted objects assigned to `states_profile` and `impact_responses`, if of the correct class
-  if(is(states_profile, "State")) states_profile <- list(states_profile)
-  if(is(driver_responses, "DriverResponse")) driver_responses <- list(driver_responses)
+  if (is(states_profile, "State")) {
+    states_profile <- list(states_profile)
+  }
+  if (is(driver_responses, "DriverResponse")) {
+    driver_responses <- list(driver_responses)
+  }
 
   # input validation ---------------
 
@@ -196,15 +190,17 @@ Species <- function(id = NA_character_,
   check_class(body_mass_distr, "VarDist")
   check_class(energy_to_mass_distr, "VarDist")
   check_class(mortality_thresh_distr, "VarDist")
-  if(length(states_profile) > 0) check_class(states_profile, "State", inlist = TRUE)
-  if(length(driver_responses) > 0) check_class(driver_responses, "DriverResponse", inlist = TRUE)
-
+  if (length(states_profile) > 0) {
+    check_class(states_profile, "State", inlist = TRUE)
+  }
+  if (length(driver_responses) > 0) {
+    check_class(driver_responses, "DriverResponse", inlist = TRUE)
+  }
 
   # TODO:
   #   (i) sense check on chosen distributions and parameters given the nature of
   #       the variable. Implied args: body_mass_distr, mortality_thresh_distr
   #   (ii) ensure behaviours are not duplicated
-
 
   # construct a new instance of <Species>
   new(
@@ -222,13 +218,12 @@ Species <- function(id = NA_character_,
 }
 
 
-
 # Validator -----------------------------------------------------
 methods::setValidity("Species", function(object) {
   errors <- character()
 
   # states_profile - state@time_budgets checks: units must be convertible across states
-  if( length(object@states_profile) > 1){
+  if (length(object@states_profile) > 1) {
     unts <- sapply(object@states_profile, \(state) state@time_budget@units)
     non_conv <- combn(unts, 2, \(x) !units::ud_are_convertible(x[[1]], x[[2]]))
     if (any(non_conv)) {
@@ -240,11 +235,12 @@ methods::setValidity("Species", function(object) {
     }
   }
 
-  if(length(errors) == 0) TRUE else do.call(paste, list(errors, collapse = " "))
+  if (length(errors) == 0) {
+    TRUE
+  } else {
+    do.call(paste, list(errors, collapse = " "))
+  }
 })
-
-
-
 
 
 # Methods -----------------------------------------------------
@@ -252,17 +248,10 @@ methods::setValidity("Species", function(object) {
 ## utils  ----
 
 #' @include s4_utils.R
-methods::setMethod("is_empty", "Species", function(object){
+methods::setMethod("is_empty", "Species", function(object) {
   #length(object@driver_responses) == 0
   length(object@states_profile) == 0
 })
-
-
-
-
-
-
-
 
 # TODO
 # 1. `show` method for <Species> for neat display
